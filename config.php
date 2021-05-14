86fe1ef02e89f7358f707c0f58fe529d00754be5
Adeilson Paz <adeilsonth@hotmail.com>
2021-05-14 14:26:57 -0300

    conexão banco

--- config.php
+++ config.php
@@ -1,7 +1,14 @@
 <?php
+//conexão com banco
 Function bd(){
 	define('HOST', 'localhost');
 	define('BD', 'vies');
 	define('USER', 'root');
 	define('PASS','')
+	try {
+		$pdo=new pdo('mysql:host='HOST.';dbname='.BD,USER,PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8"));
+		$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);	
+	} catch (Exception $e) {
+		echo "Erro na conexão".$e;
+	}
 }
