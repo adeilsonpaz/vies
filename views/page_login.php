f12e9b76e42f9f7b0fef8c9f8c98b3b2c9693dfe
Adeilson Paz <adeilsonth@hotmail.com>
2021-05-18 11:34:57 -0300

    alter

--- views/page_login.php
+++ views/page_login.php
@@ -111,39 +111,51 @@
             }
         </style>
     </head>
-  <body class="text-center">
-    <div class="container-fluid px-1 px-md-5 px-lg-1 px-xl-5 py-5 mx-auto">
-    <div class="card card0 border-0">
-        <div class="row d-flex">
-            <div class="col-lg-6" style="background: #4196e1;">
-                <div class="card1 pb-5">
-                    <div class="row px-3 justify-content-center mt-4 mb-5 border-line"> <img src="./images/logo_transparent.png" class="image"> </div>
+    <body class="text-center">
+        <div class="container-fluid px-1 px-md-5 px-lg-1 px-xl-5 py-5 mx-auto">
+            <div class="card card0 border-0">
+                <div class="row d-flex">
+                    <div class="col-lg-6" style="background: #4196e1;">
+                        <div class="card1 pb-5">
+                            <div class="row px-3 justify-content-center mt-4 mb-5 border-line"> <img src="./images/logo_transparent.png" class="image"> </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="card2 card border-0 px-4 py-5">
+                            <div class="logi">
+                                <h2>VIES - Login System</h2>                        
+                            </div>
+                            <div class="row px-3"> <label class="mb-1">
+                                <h6 class="mb-0 text-sm">Email Address</h6>
+                                </label><input class="mb-4" type="text" name="email" placeholder="Entrar com email">
+                            </div>
+                            <div class="row px-3">
+                                <label class="mb-1">
+                                    <h6 class="mb-0 text-sm">Password</h6>
+                                </label>
+                                <input type="password" name="password" placeholder="Entrar com senha">
+                            </div>
+                            <div class="row px-3 mb-4">
+                                <div class="custom-control custom-checkbox custom-control-inline">
+                                    <input id="chk1" type="checkbox" name="chk" class="custom-control-input">
+                                    <label for="chk1" class="custom-control-label text-sm">Lembrar</label>
+                                </div><a href="#" class="ml-auto mb-0 text-sm">Esqueceu a senha?</a>
+                            </div>
+                            <div class="row mb-3 px-3">
+                                <button type="submit" class="btn btn-blue text-center">Login</button>
+                            </div>
+                            <div class="row mb-4 px-3">
+                                <small class="font-weight-bold">Não tem uma conta? <a class="text-danger ">Register</a></small>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div class="col-lg-6">
-                <div class="card2 card border-0 px-4 py-5">
-                    <div class="logi">
-                        <h2>VIES - Login System</h2>                        
+                <div class="bg-blue py-4">
+                    <div class="row px-3">
+                        <small class="ml-4 ml-sm-5 mb-2">Vies - Copyright &copy; 2019. Todos os Direitos Reservados.</small>
                     </div>
-                    <div class="row px-3"> <label class="mb-1">
-                            <h6 class="mb-0 text-sm">Email Address</h6>
-                        </label> <input class="mb-4" type="text" name="email" placeholder="Entrar com email"> </div>
-                    <div class="row px-3"> <label class="mb-1">
-                            <h6 class="mb-0 text-sm">Password</h6>
-                        </label> <input type="password" name="password" placeholder="Entrar com senha"> </div>
-                    <div class="row px-3 mb-4">
-                        <div class="custom-control custom-checkbox custom-control-inline"> <input id="chk1" type="checkbox" name="chk" class="custom-control-input"> <label for="chk1" class="custom-control-label text-sm">Lembrar</label> </div> <a href="#" class="ml-auto mb-0 text-sm">Esqueceu a senha?</a>
-                    </div>
-                    <div class="row mb-3 px-3"> <button type="submit" class="btn btn-blue text-center">Login</button> </div>
-                    <div class="row mb-4 px-3"> <small class="font-weight-bold">Não tem uma conta? <a class="text-danger ">Register</a></small> </div>
                 </div>
             </div>
         </div>
-        <div class="bg-blue py-4">
-            <div class="row px-3"> <small class="ml-4 ml-sm-5 mb-2">Vies - Copyright &copy; 2019. Todos os Direitos Reservados.</small>
-            </div>
-        </div>
-    </div>
-</div>
-  </body>
+    </body>
 </html>
